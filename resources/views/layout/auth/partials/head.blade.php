7fccff2eb56eae274885d48e4b92eaed5e6789c6
dev-shuvo <coredeveloper.2013@gmail.com>
2024-02-15 03:01:15 +0600

    feat: new template added and done for admin layout design

---
+++ resources/views/layout/auth/partials/head.blade.php
@@ -0,0 +1,5 @@
+<meta charset="utf-8">
+<title>Mosaic HTML Demo - Sign in</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link href="{{ asset('css/vendors/flatpickr.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
