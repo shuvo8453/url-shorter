7f46949927230c8fc65ff5905a63961acdf59e83
dev-shuvo <saifulshuvo2@gmail.com>
2024-03-28 01:23:37 +0600

    remove all the vue related unnecessary time consuming task.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistrationRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+
+    }
+
+    public function registation(RegistrationRequest $registrationRequest)
+    {
+        // $registrationRequest->
+    }
+}
